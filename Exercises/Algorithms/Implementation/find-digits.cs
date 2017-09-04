2abba7fcc9a61676ff2ee032fd257ccebea0fcae
Trevor <trevor@nextscape.net>
2017-09-04 19:23:58 +0900

    Finished 2 more (up to find-digits)

---
+++ Exercises/Algorithms/Implementation/find-digits.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/find-digits
+    /// Given an integer, N, traverse its digits (1,2,...,n) and determine how many digits evenly divide N (i.e.: count the number of times N divided by each digit i has a remainder of 0). Print the number of evenly divisible digits.
+    /// Note: Each digit is considered to be unique, so each occurrence of the same evenly divisible digit should be counted(i.e.: for , the answer is ).
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            string originalNum = Console.ReadLine();
+            int n = Convert.ToInt32(originalNum);
+            int dividedNums = 0;
+            for (int i = 0; i < originalNum.Length; i++)
+            {
+                int checkNum = Int32.Parse(originalNum[i].ToString());
+                //Console.WriteLine($"Checking if the element [{checkNum}] is part of [{originalNum}]");
+                if (checkNum == 0)
+                {
+                    continue;
+                }
+                if (n % checkNum == 0)
+                {
+                    dividedNums++;
+                }
+            }
+            Console.WriteLine(dividedNums);
+        }
+    }
+}
---
+++ Exercises/Algorithms/Implementation/jumping-on-the-clouds-revisited.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited
+    /// Aerith is playing a cloud game! In this game, there are n clouds numbered sequentially from 0 to n-1. Each cloud is either an ordinary cloud or a thundercloud.
+    /// Aerith starts out on cloud 0 with energy level E=100.She can use 1 unit of energy to make a jump of size k to cloud (i+k) % n, and she jumps until she gets back to cloud 0.If Aerith lands on a thundercloud, her energy (E) decreases by 2 additional units.The game ends when Aerith lands back on cloud 0.
+    /// Given the values of n, k, and the configuration of the clouds, can you determine the final value of E after the game ends?
+    /// If ci=0, then cloud i is an ordinary cloud.
+    /// If ci=1, then cloud i is a thundercloud.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(String[] args)
+    {
+        string[] tokens_n = Console.ReadLine().Split(' ');
+        int numClouds = Convert.ToInt32(tokens_n[0]);
+        int jumpsize = Convert.ToInt32(tokens_n[1]);
+        string[] c_temp = Console.ReadLine().Split(' ');
+        int[] clouds = Array.ConvertAll(c_temp, Int32.Parse);
+
+        int currentEnergy = 100;
+        int currentCloudIndex = 0;
+        while (currentEnergy == 100 || (currentEnergy > 0 && currentCloudIndex != 0))
+        {
+            currentCloudIndex = (currentCloudIndex + jumpsize) % (numClouds);
+            currentEnergy -= (1 + (clouds[currentCloudIndex] * 2));
+            //Console.WriteLine($"Jumped to index [{currentCloudIndex}] where the val is [{clouds[currentCloudIndex]}], current energy is now [{currentEnergy}]");
+        }
+
+        Console.WriteLine(currentEnergy);
+    }
+}
