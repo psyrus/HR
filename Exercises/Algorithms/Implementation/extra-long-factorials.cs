d7e6031df6c0dd081484b142c662d7be5f477d85
Trevor <revolution.x@gmail.com>
2017-09-06 22:03:07 +0900

    extra-long-factorials

---
+++ Exercises/Algorithms/Implementation/extra-long-factorials.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+class Solution
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/extra-long-factorials
+    /// You are given an integer N. Print the factorial of this number.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        BigInteger factorial = new BigInteger(n);
+
+        int counter = 1;
+        while (counter < n)
+        {
+            factorial = factorial * (n - counter);
+            counter++;
+        }
+
+        Console.WriteLine(factorial);
+    }
+}
