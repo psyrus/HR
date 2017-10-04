cbce361eb89f971a008e46a08812d387dfd976ba
Trevor <revolution.x@gmail.com>
2017-10-04 23:47:01 +0900

    Finished up to lisa-workbook

---
+++ Exercises/Algorithms/Implementation/chocolate-feast.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/chocolate-feast
+    /// Little Bobby loves chocolate, and he frequently goes to his favorite 5&10 store, Penny Auntie, with n dollars to buy chocolates. Each chocolate has a flat cost of c dollars, and the store has a promotion where they allow you to trade in m chocolate wrappers in exchange for 1 free piece of chocolate.
+    /// For example, if m=2 and Bobby has dollars that he uses to buy 4 chocolates at dollar c=1 apiece, he can trade in the 4 wrappers to buy 2 more chocolates.Now he has 2 more wrappers that he can trade in for 1 more chocolate. Because he only has 1 wrapper left at this point and 1 lt m, he was only able to eat a total of 7 pieces of chocolate.
+    /// Given n, c, and m for t trips to the store, can you determine how many chocolates Bobby eats during each trip?
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int dollars = Convert.ToInt32(tokens_n[0]);
+            int chocolatePrice = Convert.ToInt32(tokens_n[1]);
+            int wrappersNeeded = Convert.ToInt32(tokens_n[2]);
+            int leftoverWrappers = 0;
+
+            int chocolatesPurchased = dollars / chocolatePrice;
+            leftoverWrappers += chocolatesPurchased;
+
+            //After the initial purchase, keep looping to redeem wrappers for more chocolate until no more wrappers can be redeemed
+            int redeemedChocolates = 0;
+            while (leftoverWrappers >= wrappersNeeded)
+            {
+                redeemedChocolates += leftoverWrappers / wrappersNeeded;
+                leftoverWrappers = (leftoverWrappers % wrappersNeeded) + (leftoverWrappers / wrappersNeeded);
+            }
+
+
+            Console.WriteLine(chocolatesPurchased + redeemedChocolates);
+        }
+    }
+}
---
+++ Exercises/Algorithms/Implementation/lisa-workbook.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/lisa-workbook
+    /// Lisa just got a new math workbook. A workbook contains exercise problems, grouped into chapters.
+    /// There are n chapters in Lisa's workbook, numbered from 1 to n.
+    /// The i-th chapter has ti problems, numbered from 1 to ti.
+    /// Each page can hold up to k problems. There are no empty pages or unnecessary spaces, so only the last page of a chapter may contain fewer than k problems.
+    /// Each new chapter starts on a new page, so a page will never contain problems from more than one chapter.
+    /// The page number indexing starts at 1.
+    /// Lisa believes a problem to be special if its index (within a chapter) is the same as the page number where it's located. Given the details for Lisa's workbook, can you count its number of special problems?
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        string[] firstLine = Console.ReadLine().Split(' ');
+        string[] secondLine = Console.ReadLine().Split(' ');
+
+        int numChapters = int.Parse(firstLine[0]);
+        int problemsPerChapterPage = int.Parse(firstLine[1]);
+
+        int specialProblemsCount = 0;
+        int totalPagesPassed = 1;
+        foreach (var item in secondLine)
+        {
+            int problems = int.Parse(item);
+            Chapter thisChapter = new Chapter(problemsPerChapterPage, problems);
+            specialProblemsCount += thisChapter.GetTotalSpecial(totalPagesPassed);
+            totalPagesPassed += thisChapter.totalPages;
+        }
+
+        Console.WriteLine(specialProblemsCount);
+    }
+
+    class Chapter
+    {
+        int problemsPerPage;
+        int totalProblems;
+        public int totalPages;
+        int overflowProblems;
+        int chapterNum;
+        public Chapter(int problemsPerPage, int totalProblems)
+        {
+            this.problemsPerPage = problemsPerPage;
+            this.totalProblems = totalProblems;
+
+            overflowProblems = totalProblems % problemsPerPage;
+            totalPages = totalProblems / problemsPerPage;
+            if (overflowProblems > 0)
+            {
+                totalPages += 1;
+            }
+        }
+
+        internal int GetTotalSpecial(int chapterStartPage)
+        {
+            int totalSpecial = 0;
+            if (chapterStartPage > totalProblems)
+            {
+                return totalSpecial;
+            }
+
+            for (int pageOffset = 0; pageOffset < totalPages; pageOffset++)
+            {
+                int currentPage = chapterStartPage + pageOffset;
+                int minProbIndex = pageOffset * problemsPerPage + 1;
+                int maxProbIndex = (pageOffset + 1) * problemsPerPage;
+
+                if (currentPage >= minProbIndex && currentPage <= maxProbIndex)
+                {
+                    totalSpecial++;
+                }
+            }
+            return totalSpecial;
+        }
+    }
+}
