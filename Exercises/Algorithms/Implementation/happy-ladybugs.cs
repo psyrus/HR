caa8c262a50bc6539ce88be287f049598096e4b8
Trevor <revolution.x@gmail.com>
2017-11-09 00:32:58 +0900

    Finished "happy ladybugs" and "manasa and stones"

---
+++ Exercises/Algorithms/Implementation/happy-ladybugs.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/happy-ladybugs/problem
+    /// Happy Ladybugs is a board game having the following properties:
+    /// - The board is represented by a string, b, of length n.The ith character of the string, bi, denotes the ith cell of the board.
+    /// -- If bi is an underscore (i.e., _), it means the ith cell of the board is empty.
+    /// -- If bi is an uppercase English alphabetic letter (i.e., A through Z), it means the ith cell contains a ladybug of color bi.
+    /// -- String b will not contain any other characters.
+    /// - A ladybug is happy only when its left or right adjacent cell (i.e.,bi+/-1 ) is occupied by another ladybug having the same color.
+    /// - In a single move, you can move a ladybug from its current position to any empty cell.
+    /// Given the values of  and  for  games of Happy Ladybugs, determine if it's possible to make all the ladybugs happy. For each game, print YES on a new line if all the ladybugs can be made happy through some number of moves; otherwise, print NO to indicate that no number of moves will result in all the ladybugs being happy.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(String[] args)
+    {
+        int numGames = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < numGames; a0++)
+        {
+            int numCells = Convert.ToInt32(Console.ReadLine());
+            string grid = Console.ReadLine();
+
+            Dictionary<char, int> types = new Dictionary<char, int>();
+            char previousItem = ' ';
+            int minConcurrent = numCells;
+            int concurrentCount = 0;
+            for (int i = 0; i < numCells; i++)
+            {
+                char item = grid[i];
+
+                //If the char has not yet been seen, add it. Increment seen values.
+                if (!types.ContainsKey(item))
+                {
+                    types.Add(item, 0);
+                }
+                types[item]++;
+
+                //Check if this is a perfect set (pairs or more of each ladybug next to each other)
+                if (previousItem != item && concurrentCount > 0)
+                {
+                    if (concurrentCount < minConcurrent)
+                    {
+                        minConcurrent = concurrentCount;
+                    }
+                    concurrentCount = 1;
+                }
+                else
+                {
+                    concurrentCount++;
+
+                }
+                previousItem = item;
+
+            }
+
+            //Final check on the last character to see if it's not a loner.
+            if (concurrentCount < minConcurrent)
+            {
+                minConcurrent = concurrentCount;
+            }
+
+            //If there is at least one space, >1 of all bug types OR no ladybugs. OR if it is already a perfect set (minConcurrent > 1)
+            if (types.ContainsKey('_'))
+            {
+                if (types.Keys.Count < 2 || AllOk(types))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+
+                }
+            }
+            else if (minConcurrent > 1)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+
+    private static bool AllOk(Dictionary<char, int> bugCounts)
+    {
+        foreach (var key in bugCounts.Keys)
+        {
+            if (bugCounts[key] == 1 && key != '_')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Exercises/Algorithms/Implementation/manasa-and-stones.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/manasa-and-stones/problem
+    /// Manasa is out on a hike with friends. She finds a trail of stones with numbers on them. She starts following the trail and notices that two consecutive stones have a difference of either a or b. Legend has it that there is a treasure trove at the end of the trail and if Manasa can guess the value of the last stone, the treasure would be hers. Given that the number on the first stone was 0, find all the possible values for the number on the last stone.
+    /// Note: The numbers on the stones are in increasing order.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int numTestCases = int.Parse(Console.ReadLine());
+        for (int i = 0; i < numTestCases; i++)
+        {
+            int numStones = int.Parse(Console.ReadLine());
+            int minIncrement = int.Parse(Console.ReadLine());
+            int maxIncrement = int.Parse(Console.ReadLine());
+            List<int> totals = new List<int>();
+            HashSet<int> seen = new HashSet<int>();
+
+            for (int j = 0; j < numStones; j++)
+            {
+                int value = maxIncrement * j + minIncrement * (numStones - j - 1);
+                if (!seen.Contains(value))
+                {
+                    seen.Add(value);
+                    totals.Add(value);
+                }
+            }
+
+            totals.Sort();
+            Console.WriteLine(String.Join(" ", totals));
+        }
+    }
+}
