105d415e906f0a2678939b834ba1f66afcb1132d
Trevor <revolution.x@gmail.com>
2018-12-24 16:57:55 +0900

    Finished a couple of easy array questions

---
+++ Exercises/Data Structures/Arrays/2d-array.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+/// <summary>
+/// https://www.hackerrank.com/challenges/2d-array
+/// Given a 6x6 2D Array, arr
+/// We define an hourglass in A to be a subset of values with indices falling in this pattern in arr's graphical representation...
+/// 
+/// There are 16 hourglasses in arr, and an hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in arr, then print the maximum hourglass sum.
+/// </summary>
+class Solution
+{
+    static int ARRAY_WIDTH = 6;
+    static int ARRAY_HEIGHT = 6;
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr)
+    {
+        int maxSum = int.MinValue;
+        for (int arrRow = 0; arrRow <= ARRAY_HEIGHT / 2; arrRow++)
+        {
+            for (int arrColumn = 0; arrColumn <= ARRAY_WIDTH / 2; arrColumn++)
+            {
+                Console.WriteLine($"Evaluating the I starting from [{arrRow}][{arrColumn}]");
+                int thisSum = singleHourglassSum(arr, arrRow, arrColumn);
+                Console.WriteLine($"Sum is: {thisSum}");
+                if (thisSum > maxSum)
+                {
+                    maxSum = thisSum;
+                }
+            }
+        }
+        return maxSum;
+    }
+
+    /// <summary>
+    /// Function to calculate the sum of an hourglass in a 2d array.
+    /// Assumes index is legitimate start of an hourglass (within bounds)
+    /// </summary>
+    /// <param name="arr">The array to check</param>
+    /// <param name="col">Starting X coordinate from top left</param>
+    /// <param name="row">Starting Y coordinate from top left</param>
+    /// <returns>Sum of hourglass values.</returns>
+    static int singleHourglassSum(int[][] arr, int col, int row)
+    {
+        return arr[row][col] + arr[row][col + 1] + arr[row][col + 2] + arr[row + 1][col + 1] + arr[row + 2][col] + arr[row + 2][col + 1] + arr[row + 2][col + 2];
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int[][] arr = new int[6][];
+
+        for (int i = 0; i < 6; i++)
+        {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
+
+        int result = hourglassSum(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Exercises/Data Structures/Arrays/array-ds.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+/// <summary>
+/// https://www.hackerrank.com/challenges/arrays-ds/problem
+/// An array is a type of data structure that stores elements of the same type in a contiguous block of memory.
+/// In an array, A, of size N, each memory location has some unique index, I (where 0 lte i lte N), that can be referenced as A[I]
+/// (you may also see it written as Ai).
+/// Given an array, A, of N integers, print each element in reverse order as a single line of space-separated integers.
+/// </summary>
+class Solution
+{
+
+    // Complete the reverseArray function below.
+    static int[] reverseArray(int[] a)
+    {
+        int[] newArray = new int[a.Length];
+        for (int i = 0; i < a.Length; i++)
+        {
+            int index = a.Length - 1 - i;
+            newArray[index] = a[i];
+        }
+        return newArray;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int[] res = reverseArray(arr);
+
+        textWriter.WriteLine(string.Join(" ", res));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
