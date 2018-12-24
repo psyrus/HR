18989c0be72890dee95349a64e347f00eb8fc7a3
Trevor <revolution.x@gmail.com>
2018-12-24 15:57:42 +0900

    Finished Halloween Sale

---
+++ Exercises/Algorithms/Implementation/halloween-sale.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Exercises.Algorithms.Implementation
+{
+    /// <summary>
+    /// You wish to buy video games from the famous online video game store Mist.
+    /// Usually, all games are sold at the same price, P dollars.
+    /// However, they are planning to have the seasonal Halloween Sale next month in which you can buy games at a cheaper price.
+    /// Specifically, the first game you buy during the sale will be sold at P dollars, but every subsequent game you buy
+    /// will be sold at exactly D dollars less than the cost of the previous one you bought.
+    /// This will continue until the cost becomes less than or equal to M dollars, after which every game you buy will cost M dollars each.
+    /// </summary>
+    class Solution
+    {
+
+        // Complete the howManyGames function below.
+        static int HowManyGames(int p, int d, int m, int s)
+        {
+            int totalCashRemaining = s;
+            int numberOfGames = 0;
+            int currentCost = p;
+            // Return the number of games you can buy
+            while (totalCashRemaining > 0)
+            {
+                if (totalCashRemaining < currentCost)
+                {
+                    break;
+                }
+                numberOfGames++;
+                Console.WriteLine($"Game #{numberOfGames} purchased at {currentCost}");
+                totalCashRemaining -= currentCost;
+                Console.WriteLine($"Total cash remaining: {totalCashRemaining}");
+
+                currentCost = currentCost <= m ? m : currentCost - d;
+
+
+            }
+            return numberOfGames;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] pdms = Console.ReadLine().Split(' ');
+
+            int p = Convert.ToInt32(pdms[0]);
+
+            int d = Convert.ToInt32(pdms[1]);
+
+            int m = Convert.ToInt32(pdms[2]);
+
+            int s = Convert.ToInt32(pdms[3]);
+
+            int answer = HowManyGames(p, d, m, s);
+
+            textWriter.WriteLine(answer);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
---
+++ Exercises/Exam.cs
@@ -0,0 +1,102 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    // Complete the maxRegion function below.
+    static int maxRegion(int[][] grid)
+    {
+        HashSet<Tuple<int, int>> visited = new HashSet<Tuple<int, int>>();
+        int maxGridSize = 0;
+        for (int i = 0; i < grid.Count(); i++)
+        {
+            for (int j = 0; j < grid[i].Count(); j++)
+            {
+                if (visited.Contains(new Tuple<int, int>(i, j)))
+                {
+                    continue;
+                }
+                if (grid[i][j] == 1)
+                {
+                    int gs = FindGridSize(grid, visited, i, j);
+                    if (maxGridSize < gs)
+                    {
+                        maxGridSize = gs;
+                    }
+                }
+            }
+        }
+        return maxGridSize;
+
+    }
+
+    static int FindGridSize(int[][] grid, HashSet<Tuple<int, int>> visited, int x, int y)
+    {
+        int totalInGrid = 0;
+        foreach (int[] pair in GetAdjacent(grid, x, y))
+        {
+            visited.Add(new Tuple<int, int>(pair[0], pair[1]));
+            if (grid[pair[0]][pair[1]] == 1)
+            {
+                totalInGrid++;
+                totalInGrid += FindGridSize(grid, visited, pair[0], pair[1]);
+            }
+        }
+        return totalInGrid;
+    }
+
+    static int[][] GetAdjacent(int[][] grid, int x, int y)
+    {
+        List<int, int> items = new List<int, int>();
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0)
+                {
+                    continue;
+                }
+                if (x + i >= 0 && y + j >= 0 && x + i < grid.Count() && y + j < grid[0].Count() && !visited.Contains((x + i, y + j)))
+                {
+                    items.Add((x + i, y + j));
+                }
+            }
+        }
+
+        return items.ToArray();
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        int[][] grid = new int[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            grid[i] = Array.ConvertAll(Console.ReadLine().Split(' '), gridTemp => Convert.ToInt32(gridTemp));
+        }
+
+        int res = maxRegion(grid);
+
+        textWriter.WriteLine(res);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
