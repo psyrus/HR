972fd55e376ef60bbb893f279084b2c0a1d8e6d0
Trevor Chinn <revolution.x@gmail.com>
2022-01-06 23:04:48 +0900

    Exercises/anagram bigger (#5)
    * Finished anagram; started bigger
    
    * Got bigger working for 80% of use cases
    
    * part way through refactor
    
    * Finish up
    
    * bit more
    
    * Based on learning from website, improved algorithm

---
+++ Exercises/Algorithms/Implementation/bigger-is-greater.py
@@ -0,0 +1,120 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'biggerIsGreater' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING w as parameter.
+# https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+
+
+def biggerIsGreater(w):
+    # Define loop indeces:
+    rightmostDecendingSequenceStartIndex = len(w) - 1
+    swapCandidateIndex = len(w) - 1
+
+    wordAsList = list(w)
+
+    while rightmostDecendingSequenceStartIndex > 0 and ord(w[rightmostDecendingSequenceStartIndex-1]) >= ord(w[rightmostDecendingSequenceStartIndex]):
+        rightmostDecendingSequenceStartIndex -= 1
+
+    if rightmostDecendingSequenceStartIndex <= 0:
+        return "no answer"
+
+    swapTargetIndex = rightmostDecendingSequenceStartIndex - 1
+
+    while swapCandidateIndex > 0 and ord(w[swapCandidateIndex]) <= ord(w[swapTargetIndex]):  # Why i - 1?
+        swapCandidateIndex -= 1
+
+    temp = wordAsList[swapTargetIndex]
+    wordAsList[swapTargetIndex] = wordAsList[swapCandidateIndex]
+    wordAsList[swapCandidateIndex] = temp
+
+    sortedSuffix = wordAsList[rightmostDecendingSequenceStartIndex:]
+    sortedSuffix.reverse()
+
+    return ''.join(wordAsList[:rightmostDecendingSequenceStartIndex] + sortedSuffix)
+
+
+def biggerIsGreaterOldBadWay(w):
+    # Start from the right side of the string, checking each char to see if there is something LOWER than it in value that can be swapped out to make the overall total rise (the least)
+    indexSwapCandidates = []
+    wordArray = list(w)
+    largestSwapTargetIndex = 0
+    for i, e in reversed(list(enumerate(w))):
+        charVal = ord(e)
+
+        for j in range(i, -1, -1):
+            # Once something can be switched, swap them
+            if ord(w[j]) < charVal:
+                # print(f"For string {w}, character {e} at position {i} could be swapped with character {w[j]} at position {j} because {ord(w[j])} is < {charVal}")
+                indexSwapCandidates.append(
+                    {
+                        "sourceIndex": i,
+                        "sourceValue": w[i],
+                        "sourceValueInt": ord(w[i]),
+                        "targetIndex": j,
+                        "targetValue": w[j],
+                        "targetValueInt": ord(w[j]),
+                    }
+                )
+                if j > largestSwapTargetIndex:
+                    largestSwapTargetIndex = j
+        # Need to break early if we have exceeded the meaningful number of items to check
+        if i < largestSwapTargetIndex:
+            # print(f"Reached iteration (i) of {i} but the possibleSwapIndex includes {largestSwapTargetIndex} so exiting loop early")
+            break
+    if not indexSwapCandidates:
+        return "no answer"
+
+    # Do the swap with the best candidate
+    newlist = sorted(indexSwapCandidates,
+                     key=lambda d: d["targetIndex"], reverse=True)
+    swapItem = newlist[0]
+    temp = wordArray[swapItem["sourceIndex"]]
+    wordArray[swapItem["sourceIndex"]] = wordArray[swapItem["targetIndex"]]
+    wordArray[swapItem["targetIndex"]] = temp
+
+    # Then sort the items AFTER the left most swapped index fromm small to large
+    sortedSubArray = wordArray[swapItem["targetIndex"] + 1:]
+    sortedSubArray.sort()
+    return "".join(wordArray[: swapItem["targetIndex"] + 1] + sortedSubArray)
+
+
+if __name__ == "__main__":
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    # T = int(input().strip())
+
+    # for T_itr in range(T):
+    #     w = input()
+
+    #     result = biggerIsGreater(w)
+
+    #     fptr.write(result + '\n')
+
+    # fptr.close().
+
+    cases = [
+        "bb",
+        "lmno",
+        "dkhc"
+    ]
+
+    expectedOutput = [
+        "no answer",
+        "lmon",
+        "hcdk"
+    ]
+
+    for i, c in enumerate(cases):
+        result = biggerIsGreater(c)
+        if expectedOutput[i] != result:
+            print(
+                f"Case ({i}): \n\t[{c}]\n\t[{expectedOutput[i]}] ✅\n\t[{result}] ✖")
